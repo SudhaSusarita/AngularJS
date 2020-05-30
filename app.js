bd5eae00c4e491997235e2533ac538cc2c23a695
Sudha Susarita <sudhasusarita@gmail.com>
2020-05-30 15:31:18 +0100

    Assignment1 Coursera

---
+++ app.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+    
+    angular.module('Application', [])
+.controller("AppController",function($scope){
+            console.log("Fsdf");
+    $scope.items="ddd";
+    $scope.checkIfTooMuch = function(){
+        console.log("erw");
+
+    $scope.message = messageGenerator($scope.items);
+    }
+    });
+
+    function messageGenerator(items){
+       
+        let itemEntries = items.split(',');
+        let noOfItems = itemEntries.length;
+        let noOfEmpty=0;
+        for(var i=0;i<noOfItems;i++){
+        
+            if(itemEntries[i].trim().length==0){
+                noOfEmpty++;
+            }
+        }
+        if(noOfEmpty!=0){
+            noOfItems = noOfItems-noOfEmpty;
+        }
+        let message="";
+        if(noOfItems==0 || items==""){
+            message="Please enter data first";
+        }
+        else if(noOfItems<=3){
+             message="Enjoy!";
+
+        }else if(noOfItems>3 && noOfItems!=0){
+            message ="Too much!";
+        }
+        return message;
+    }
+
+})();
+
